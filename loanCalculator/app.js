578e681dae9652ee1ad88b084ea068761ad4b66f
apautvein <apautvein@gmail.com>
2020-08-18 10:47:26 -0500

    Updated loader and user experience

--- loanCalculator/app.js
+++ loanCalculator/app.js
@@ -1,7 +1,16 @@
 //listen for submit 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+    //hide results 
+    document.getElementById('results').style.display = 'none';
+    //show loader 
+    document.getElementById('loading').style.display = 'block';
 
-function calculateResults(e) {
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+function calculateResults() {
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
@@ -21,15 +30,23 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        document.getElementById('results').style.display = 'block';
+
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your numbers');
     }
 
-    e.preventDefault();
+
 }
 
 //show error 
 function showError(error) {
+
+    document.getElementById('results').style.display = 'none';
+
+    document.getElementById('loading').style.display = 'none';
     //create a div 
     const errorDiv = document.createElement('div');
 
