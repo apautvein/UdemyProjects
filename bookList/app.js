427012ad36c5494d87635bb71ff141fb6c506f62
apautvein <apautvein@gmail.com>
2020-08-24 11:32:22 -0500

    Delete a book from the list

--- bookList/app.js
+++ bookList/app.js
@@ -33,6 +33,13 @@ UI.prototype.showAlert = function (message, className) {
 }
 
 
+UI.prototype.deleteBook = function (target) {
+    if (target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+}
+
+
 UI.prototype.clearFields = function () {
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
@@ -59,4 +66,14 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     }
 
     e.preventDefault();
-}); 
+});
+
+
+document.getElementById('book-list').addEventListener('click', function (e) {
+    const ui = new UI();
+
+    ui.deleteBook(e.target);
+    ui.showAlert('Book Removed!', 'success');
+
+    e.preventDefault();
+})
