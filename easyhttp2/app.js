7ac57956184fb45faef0ce56f597679cdfb583f5
apautvein <apautvein@gmail.com>
2020-08-25 15:09:20 -0500

    Updated custom HTTP library using fetch, ES6 classes, and promises- get and post requests

---
+++ easyhttp2/app.js
@@ -0,0 +1,16 @@
+const http = new EasyHTTP;
+
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err)); 
+
+
+const data = {
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'jdoe@gmail.com'
+}
+
+http.post('https://jsonplaceholder.typicode.com/users', data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err)); 
