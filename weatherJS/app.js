b88b7fb5015027f116a59952592e5e9b4142ffbf
apautvein <apautvein@gmail.com>
2020-08-26 15:53:41 -0500

    Display the weather

--- weatherJS/app.js
+++ weatherJS/app.js
@@ -1,5 +1,7 @@
 const weather = new Weather('Boston', 'US');
 
+const ui = new UI();
+
 
 document.addEventListener('DOMContentLoaded', getWeather);
 // weather.changeLocation('Miami', 'US');
@@ -7,7 +9,7 @@ document.addEventListener('DOMContentLoaded', getWeather);
 function getWeather() {
     weather.getWeather()
         .then(results => {
-            console.log(results);
+            ui.paint(results);
         })
         .catch(err => console.log(err));
 
