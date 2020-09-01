769865b218fa639214ae323b5b326c3daaeaab85
apautvein <apautvein@gmail.com>
2020-09-01 10:07:02 -0500

    Get and populate items

--- tracalorieApp/app.js
+++ tracalorieApp/app.js
@@ -21,6 +21,9 @@ const ItemCtrl = (function () {
     }
 
     return {
+        getItems: function () {
+            return data.items;
+        },
         logData: function () {
             return data;
         }
@@ -29,10 +32,23 @@ const ItemCtrl = (function () {
 
 // UI Controller
 const UICtrl = (function () {
-
+    const UISelectors = {
+        itemList: '#item-list'
+    }
 
     return {
+        populateItemList: function (items) {
+            let html = '';
+            items.forEach(function (item) {
+                html += `<li class="collection-item" id="${item.id}">
+                <strong>${item.name} </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content"><i class="edit-item fa fa-pencil"></i></a>
+            </li>`;
+            });
 
+            //Insert list items 
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
     }
 })();
 
@@ -42,7 +58,10 @@ const App = (function (ItemCtrl, UICtrl) {
     //Return- public methods
     return {
         init: function () {
-            console.log('Initializing App....')
+            //Fetch items from data structure  
+            const items = ItemCtrl.getItems();
+            //Populate list with items 
+            UICtrl.populateItemList(items);
         }
     }
 
